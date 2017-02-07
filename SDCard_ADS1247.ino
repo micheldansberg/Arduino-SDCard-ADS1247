f3f7ba478585d7231b7698631e2a27dd8737854e
Michel Dansberg <micheldansberg@hotmail.com>
2017-02-07 20:10:29 +0100

    runs on PCB

--- SDCard_ADS1247.ino
+++ SDCard_ADS1247.ino
@@ -2,14 +2,21 @@
 #include <SPI.h>
 #include <SD.h>
 
+#define hallPin 2
 #define ledPin 13
+#define SDchipSelect 4
+#define ADS1247CSpin 10   // Slave select pin for SPI communication to ADS1248.
+#define DRDYpin 3         // interrupt pin - data ready ADS1248
 
-SPISettings SDSPIsettings = SPISettings(1000000, MSBFIRST, SPI_MODE0);
 
-const int SDchipSelect = 4;
+boolean serialPCEnabled = true;
 
-byte ADS1247CSpin = 10;  // Slave select pin for SPI communication to ADS1248.
-byte DRDYpin = 3; // interrupt pin - data ready ADS1248
+volatile boolean hall = false;  // variable is set true by interrupt
+volatile unsigned long cadenceInterruptTime = millis();
+unsigned int dTcadence = 0;
+unsigned long oldCadenceInterruptTime = 0;
+
+// initialize ADS1247 class
 ADS1247 ads1247;
 
 unsigned long time = 0;
@@ -18,32 +25,45 @@ int dT = 0;
 
 void setup() {
   //Enable Serial to PC (USB)
-  Serial.begin(9600);
+  if (serialPCEnabled== true) {
+    //Enable Serial to PC (USB)
+    Serial.begin(115200);
     while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+      ; // wait for serial port to connect. Needed for native USB port only
+    }
+    Serial.println("start");
   }
-  Serial.println("start");
+  // Initialize Hall sensor
+  // Enable interrupt on pin 2 for Hall sensor
+  pinMode(hallPin, INPUT);
+  attachInterrupt(0, onCadenceInterrupt, FALLING);
+  
   // initialize SPI
   SPI.begin();
   delay(50);
   
   // see if the SD card is present and can be initialized:
-  Serial.print("Initializing SD card...");
+  if (serialPCEnabled== true) {Serial.print("Initializing SD card...");}
   if (!SD.begin(SDchipSelect)) {
-    Serial.println("Card failed, or not present");
+    if (serialPCEnabled== true) {Serial.println("Card failed, or not present");}
     // don't do anything more:
     return;
   } else {
-    Serial.println("card initialized.");
+    if (serialPCEnabled== true) {Serial.println("card initialized.");}
   }
   
   // define ads1247 communication class, configures SPI connecetion on Arduino.
   // configures ads1247 for gain, SPS, channels and voltage reference
   ads1247.init(DRDYpin, ADS1247CSpin);
-  Serial.println("End init ADS1248");
+  if (serialPCEnabled== true) {Serial.println("End init ADS1248");}
 }
 
 void loop() {
+  if (hall) {
+    dTcadence = cadenceInterruptTime - oldCadenceInterruptTime;
+    oldCadenceInterruptTime = cadenceInterruptTime;
+    hall = false;
+    }
   // wait for interrupt ADS1248
   ads1247.waitForTrigger();
   // Read ADS1248 input channel and convert read value to Volts
@@ -52,27 +72,32 @@ void loop() {
   time = millis();
   int dT = time - oldTime;
   oldTime = time;
-  // Write debugging data to PC
-  writeResultSerial(inputVoltage, dT);
   
-  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+  SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
   // if the file is available, write to it:
   if (dataFile) {
     String dataString = "";
-    dataString += String(inputVoltage);
+    dataString += String(inputVoltage, 3);
     dataString += ", ";
     dataString += String(dT);
+    dataString += ", ";
+    dataString += String(dTcadence);
     dataFile.println(dataString);
     dataFile.close();
     // print to the serial port too:
-    Serial.println(dataString);
+    if (serialPCEnabled== true) {Serial.println(dataString);}
   }
   // if the file isn't open, pop up an error:
   else {
     Serial.println("error opening datalog.txt");
   }
   SPI.endTransaction();
+}
+
+void onCadenceInterrupt() {
+  hall = true;
+  cadenceInterruptTime = millis();
 }
 
 unsigned long writeResultSerial(double inV, int dT) {
